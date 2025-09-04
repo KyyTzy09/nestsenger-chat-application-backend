c29daf3f00e8197c6fe0b1171890c423b755babc
Firegame224 <xg474627@gmail.com>
2025-09-04 23:45:33 +0700

    feat(prisma): add prisma module and add user model

---
+++ prisma/migrations/20250904155533_first/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `userId` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_userName_key`(`userName`),
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
