cb486ff3fa6a03f43f7761b4edc5ec66a95319dd
Firegame224 <xg474627@gmail.com>
2026-01-17 14:28:13 +0700

    feat: setup config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,18 @@ import { join } from 'path';
 import { FriendModule } from './modules/friend/friend.module';
 import { RoomModule } from './modules/room/room.module';
 import { ChatModule } from './modules/chat/chat.module';
-import { ReactionController } from './modules/reaction/reaction.controller';
-import { ReactionService } from './modules/reaction/reaction.service';
 import { ReactionModule } from './modules/reaction/reaction.module';
 import { ReadChatModule } from './modules/readchat/readchat.module';
 import { MediaModule } from './modules/media/media.module';
 import { StatusModule } from './modules/status/status.module';
 import { ViewerModule } from './modules/viewers/viewer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ 
+      isGlobal : true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), "uploads"),
       serveRoot: "/uploads"
